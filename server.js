07f8f5ed77885608d4417955202f99da66d90532
unknown <hudsoneeto@outlook.com>
2020-03-26 17:24:56 -0300

    layout finalizado

--- server.js
+++ server.js
@@ -35,6 +35,20 @@ const ideas  = [
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A earum numquam voluptatibus consequuntur reiciendis distinctio, fuga minima modi itaque deserunt repudiandae harum provident eaque inventore dignissimos voluptates ab culpa dolorem?",
         url: "http://rocketseat.com.br"
     },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729021.svg",
+        title: "Games",
+        category: "Diversão",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A earum numquam voluptatibus consequuntur reiciendis distinctio, fuga minima modi itaque deserunt repudiandae harum provident eaque inventore dignissimos voluptates ab culpa dolorem?",
+        url: "http://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729009.svg",
+        title: "Cozinhar",
+        category: "Receitas",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A earum numquam voluptatibus consequuntur reiciendis distinctio, fuga minima modi itaque deserunt repudiandae harum provident eaque inventore dignissimos voluptates ab culpa dolorem?",
+        url: "http://rocketseat.com.br"
+    },
 ]
 
 // configurar arquivos estáticos (css,scripts, imagens)
@@ -54,12 +68,18 @@ nunjucks.configure("views", {
 //criei uma rota /
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res){
-
-    
-    return res.render("index.html", { ideas })
+    const reversedIdeas = [...ideas].reverse()
+    let lastIdeas = []
+    for (let idea of reversedIdeas) {
+        if(lastIdeas.length < 2){
+            lastIdeas.push(idea)
+        }
+    }
+    return res.render("index.html", { ideas: lastIdeas })
 })
 server.get("/ideias", function(req, res){
-    return res.render("ideias.html")
+    const reversedIdeas = [...ideas].reverse()
+    return res.render("ideias.html", {ideas: reversedIdeas})
 })
 
 //Liguei meu servidor na porta 3000
